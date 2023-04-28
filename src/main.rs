cdb4d84334a7d2abf2ecd0f822a996ddd6ed195b
Luna Harran <36711077+Sonicpineapple@users.noreply.github.com>
2023-04-28 11:02:37 +0100

    Add scramble and some base puzzles to choose from

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use eframe::egui;
+use rand::prelude::*;
 
 fn main() -> eframe::Result<()> {
     let native_options = eframe::NativeOptions::default();
@@ -20,7 +21,7 @@ impl App {
         // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
         // for e.g. egui::PaintCallback.
         Self {
-            board: Board::new(4),
+            board: Board::new(4, 3),
         }
     }
 }
@@ -50,9 +51,9 @@ struct Board {
     size: usize,
 }
 impl Board {
-    fn new(n: usize) -> Self {
+    fn new(n: usize, lim: usize) -> Self {
         Board {
-            pieces: vec![vec![Piece::default(); n]; n],
+            pieces: vec![vec![Piece::new(lim); n]; n],
             size: n,
         }
     }
@@ -73,29 +74,74 @@ impl Board {
             self.pieces[y][x - 1].change(val);
         };
     }
+
+    fn random_move(&mut self, rng: &mut ThreadRng) {
+        let x = (rng.gen::<f32>() * self.size as f32).floor() as usize;
+        let y = (rng.gen::<f32>() * self.size as f32).floor() as usize;
+        self.press(x, y);
+    }
 }
 
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         let spectrum = colorous::RED_YELLOW_GREEN;
+        egui::TopBottomPanel::top("Top").show(ctx, |ui| {
+            egui::menu::bar(ui, |ui| {
+                ui.menu_button("Puzzle", |ui| {
+                    if ui.button("Scramble").clicked() {
+                        let mut rng = thread_rng();
+                        for _ in 0..100 {
+                            self.board.random_move(&mut rng);
+                        }
+                    }
+                    for n in 3..=6 {
+                        for l in 1..=3 {
+                            let n_str = n.to_string();
+                            let lim = 2 * l + 1;
+                            let lim_str = lim.to_string();
+                            if ui
+                                .button("".to_string() + &n_str + "x" + &n_str + ", " + &lim_str)
+                                .clicked()
+                            {
+                                self.board = Board::new(n, lim);
+                            }
+                        }
+                    }
+                    if ui.button("3x3,3").clicked() {
+                        self.board = Board::new(3, 3);
+                    }
+                });
+            })
+        });
         egui::CentralPanel::default().show(ctx, |ui| {
-            let unit = ui.available_size_before_wrap() / self.board.size as f32;
+            let rect = ui.available_rect_before_wrap();
+            let (min, size) = (rect.left_top(), rect.size());
+            let unit = size / self.board.size as f32;
             if ui.input(|input| input.pointer.primary_pressed()) {
-                if let Some(mpos) = ctx.pointer_latest_pos() {
-                    let pos = (mpos.to_vec2() / unit).to_pos2();
-                    self.board
-                        .press(pos.x.trunc() as usize, pos.y.trunc() as usize);
-                    dbg!(&self.board);
+                if ui.ui_contains_pointer() {
+                    if let Some(mpos) = ctx.pointer_latest_pos() {
+                        let pos = ((mpos - min) / unit).to_pos2();
+                        self.board
+                            .press(pos.x.trunc() as usize, pos.y.trunc() as usize);
+                    }
                 }
             }
 
             for (j, row) in self.board.pieces.iter().enumerate() {
                 for (i, piece) in row.iter().enumerate() {
-                    let col = spectrum.eval_rational((piece.state + 1) % (piece.lim), piece.lim);
-                    let col = egui::Color32::from_rgb(col.r, col.g, col.b);
+                    let col: egui::Color32;
+                    if piece.state == 0 {
+                        col = egui::Color32::WHITE;
+                    } else {
+                        let scol = spectrum.eval_rational(
+                            (piece.lim - 3 + piece.state) % (piece.lim - 1),
+                            piece.lim - 1,
+                        );
+                        col = egui::Color32::from_rgb(scol.r, scol.g, scol.b);
+                    }
                     ui.painter().rect(
                         egui::Rect::from_min_size(
-                            egui::pos2(i as f32 * unit.x, j as f32 * unit.y),
+                            min + egui::vec2(i as f32 * unit.x, j as f32 * unit.y),
                             unit,
                         ),
                         egui::Rounding::none(),
